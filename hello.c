eb538ef863079c4cabe76a495ce27fd2a9a621b7
Lyazii Ng <lyadzii@iitk.ac.in>
2018-03-05 02:20:32 +0530

    .gitignore

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 void microkernel_sendmsg(char *);
 void main(){
 printf("Helloworld!\n");
+printf("This must be a monolithic design\n");
 microkernel_sendmsg("is more portable");
 }
 void microkernel_sendmsg(char *a){
